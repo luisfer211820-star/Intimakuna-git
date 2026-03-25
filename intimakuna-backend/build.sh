7812d7ca08c0c2d61ef90cf42bc78ce251dc160d
luisfer211820 <luisfer211820@gmail.com>
2026-03-25 11:48:35 -0500

    Crear superusuario automáticamente en el build

--- intimakuna-backend/build.sh
+++ intimakuna-backend/build.sh
@@ -8,3 +8,7 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+# Crea el superusuario automáticamente usando variables de entorno
+# (DJANGO_SUPERUSER_USERNAME, DJANGO_SUPERUSER_PASSWORD, DJANGO_SUPERUSER_EMAIL)
+python manage.py createsuperuser --no-input || true
